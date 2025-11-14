e2ab4a3abdbd034a433d88a9f939127ea646a59c
Cello <octavyom@gmail.com>
2025-11-14 15:38:19 +0700

    Data Terakhir masih jalan

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-7314bb95'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ajtirp5jtg"
+    "revision": "0.8a3gk8hdqo8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
